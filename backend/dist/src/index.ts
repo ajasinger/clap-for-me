5c71cd4261f2801481f5fcaf5ff7a25307518877
Aja Singer <ajasinger@gmail.com>
2024-08-19 18:18:53 -0400

    comment out get and uncomment routes

--- backend/dist/src/index.ts
+++ backend/dist/src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
-import authRoutes from './route/auth';
-import accomplishmentRoutes from './route/accomplishment';
+import authRoutes from './route/auth.ts';
+import accomplishmentRoutes from './route/accomplishment.ts';
 
 const app = express();
 const port = 5001;
+
+// app.get("/", (req, res) => {
+//     res.send("hello world");
+// });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/accomplishment", accomplishmentRoutes);

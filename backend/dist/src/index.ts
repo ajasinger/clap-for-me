da97c04ce3ed1f90af948c8324e383865295e37d
Aja Singer <ajasinger@gmail.com>
2024-08-19 17:34:36 -0400

    change port to variable

--- backend/dist/src/index.ts
+++ backend/dist/src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 
 const app = express();
+const port = 5001;
 
 app.get("/", (req, res) => {
     res.send("hello world");
 });
 
-app.listen(5001, () => {
-    console.log("server is running on 5001");
+app.listen(port, () => {
+    console.log(`server is running on ${port}`);
 });

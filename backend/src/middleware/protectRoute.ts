0ebef156e27bc8b71baa394b7db4606d25ec76b1
Aja Singer <ajasinger@gmail.com>
2024-09-02 18:22:06 -0400

    get jet token data

--- backend/src/middleware/protectRoute.ts
+++ backend/src/middleware/protectRoute.ts
@@ -4,8 +4,19 @@ import { Request, Response, NextFunction } from "express";
 export const protectRoute = (req: Request, res: Response, next: NextFunction) => {
     try {
 
+        //access data in cookie named "jwt"
+        const token = req.cookies.jwt;
+
+        if(!token) {
+            return res.status(401).json({ error: "Not authorized. No JWT"})
+        }
+
+        //verify jwt with secret
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+
         // NextFunction means we call the next function if this executes successfully 
         next();
+
     } catch(error: any) {
 
     }

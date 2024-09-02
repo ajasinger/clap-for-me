1fd83bfb6087522e4f548186a2d9d25cae713f3a
Aja Singer <ajasinger@gmail.com>
2024-09-02 17:58:46 -0400

    call cookieParser

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,15 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from './route/auth';
 import accomplishmentRoutes from './route/accomplishment';
-
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const app = express();
 const port = 5001;
 
-app.use(express.json()); //used to parse json request bodies
+app.use(cookieParser()); //parse cookies
+app.use(express.json()); //parse json request bodies
 
 // app.get("/", (req, res) => {
 //     res.send("hello world");

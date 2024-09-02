8cc63bd985dfb4fc8745af5a802c9bae43c0533a
Aja Singer <ajasinger@gmail.com>
2024-09-02 17:54:37 -0400

    import and call protectRoute

--- backend/src/route/auth.ts
+++ backend/src/route/auth.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import { login, logout, signup } from '../controllers/authController';
+import { login, logout, signup, getUser } from '../controllers/authController';
+import { protectRoute } from '../middleware/protectRoute';
 
 const router = express.Router();
 
 // url/api/auth/{endpoint}
 
+router.get("/me", protectRoute, getUser); //check if user is logged in
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);

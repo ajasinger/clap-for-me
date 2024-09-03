0802b94d1d41eed2ef0685013497d4da86eaaa60
Aja Singer <ajasinger@gmail.com>
2024-09-03 14:35:59 -0400

    update getUser function

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -103,6 +103,17 @@ export const getUser = async(req: Request, res: Response) => {
     try {
         const user = await prisma.user.findUnique({ where: { id:req.user.id }});
 
+        if(!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        res.status(200).json({
+            id: user.id,
+            fullName: user.fullName,
+            username: user.username,
+            email: user.email
+        });
+
     } catch(error: any) {
         console.log("error in getUser controller", error.message);
         res.status(500).json({ error: "Internal Server Error" });

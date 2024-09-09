e7d820f7446e3a081da74241b93ad9125247c001
Aja Singer <ajasinger@gmail.com>
2024-09-09 17:36:11 -0400

    start coding function newAchievement to create new db entry

--- backend/src/controllers/achievementController.ts
+++ backend/src/controllers/achievementController.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from "express";
+import prisma from '../db/prisma';
 
 export const addAchievement = async (req: Request, res: Response) => {
     try {
         const { achievement } = req.body;
         const userId = req.user.id;
+
+        const newAchievement = await prisma.achievement.create({
+            data: {
+                body: achievement, 
+                creator: userId
+            }
+        });
 
     } catch(error: any) {
         console.log("error in addAchievement", error.message);

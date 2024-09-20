d9022ef1fbe428c33435756df521561a32608ee9
Aja Singer <ajasinger@gmail.com>
2024-09-20 13:44:33 -0400

    code getAchievements function

--- backend/src/controllers/achievementController.ts
+++ backend/src/controllers/achievementController.ts
@@ -24,3 +24,25 @@ export const addAchievement = async (req: Request, res: Response) => {
         res.status(500).json({ error: "Internal server error"})
     }
 }
+
+export const getAchievements = async (req: Request, res: Response) => {
+    try {
+        const {id: userId} = req.params;
+        
+        const achievements = await prisma.achievement.findMany({
+            where: {
+                creatorId: userId,
+            },
+            select: {
+                id: true,
+                body: true
+            }
+        })
+
+        if(achievements) res.status(201).json(achievements)
+
+    } catch(error: any) {
+        console.log("error in getAchievement", error.message);
+        res.status(500).json({ error: "Internal server error"})
+    }
+}

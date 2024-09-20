7d95919b0a493fe1758a82ff1b77d47ff5fc6060
Aja Singer <ajasinger@gmail.com>
2024-09-20 13:34:48 -0400

    add get route

--- backend/src/route/accomplishment.ts
+++ backend/src/route/accomplishment.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import protectRoute from '../middleware/protectRoute';
-import { addAchievement } from '../controllers/achievementController';
+import { addAchievement, getAchievements } from '../controllers/achievementController';
 
 const router = express.Router();
 
 // /api/accomplishment/{endpoint}
 
 //if user is authenticated call addAchievement function
-router.post("/add", protectRoute, addAchievement)
+router.post("/add", protectRoute, addAchievement);
+router.get("/:id", protectRoute, getAchievements);
 
 export default router;

1cb6b39950a5f1e5bd5d32f9367b034911ae5789
Aja Singer <ajasinger@gmail.com>
2024-10-28 16:35:20 -0400

    define routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Signup from './components/Signup';
+import Login from './components/Login';
+
 function App() {
 
   return (
-    <>
-      <h1 className="text-6xl text-red-600"> hello</h1>
-    </>
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route path="/" element={ < Home /> } />
+        <Route path="/signup" element={ < Signup /> } />
+        <Route path="/login" element={ < Login /> } />
+      </Routes>
+    </div>
   )
 }
 

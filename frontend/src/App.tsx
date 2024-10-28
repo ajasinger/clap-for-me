24ecb7b0dbe47f74741aef27b5743bdb2d9d8883
Aja Singer <ajasinger@gmail.com>
2024-10-28 17:25:12 -0400

    import components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
@@ -6,12 +7,15 @@ import Login from './pages/Login';
 function App() {
 
   return (
-    <div className="p-4 h-screen flex items-center justify-center">
+    <div>
+      <Navbar />
+      <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
         <Route path="/" element={ < Home /> } />
         <Route path="/signup" element={ < Signup /> } />
         <Route path="/login" element={ < Login /> } />
       </Routes>
+      </div>
     </div>
   )
 }

45e43c26b82082d14048f36794548d20524da9cb
Aja Singer <ajasinger@gmail.com>
2024-10-29 10:51:28 -0400

    style hover state

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,11 +4,14 @@ export default function Footer() {
     return(
         <div className="bg-[#FFDB7D] p-12 flex justify-between">
             <h1 className="text-2xl font-black">FTW</h1>
-            <div className="flex flex-col gap-6">
-                <Link to={"/settings"} className="hover:font-semibold">SETTINGS</Link>
-                <Link to={"/contact"} className="hover:font-semibold">CONTACT</Link>
+            <div className="flex flex-col gap-6 font-medium">
+                <Link to={"/settings"} className="border-b-2 border-transparent hover:border-current transition duration-300 ease-in-out">
+                    SETTINGS
+                </Link>
+                <Link to={"/contact"} className="border-b-2 border-transparent hover:border-current transition duration-300 ease-in-out">
+                    CONTACT
+                </Link>
             </div>
-
         </div>
     )
 }
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,9 +5,15 @@ export default function Navbar() {
         <div className="bg-[#FFDB7D] p-6 flex items-center justify-between">
             <h1 className="text-2xl font-black">FTW</h1>
             <div className="flex gap-6 items-center font-semibold">
-                <Link to={"/settings"}>SETTINGS</Link>
-                <Link to={"/contact"}>CONTACT</Link>
-                <Link to={"/login"} className="text-white bg-black px-8 py-3 rounded-full">LOGIN</Link>
+                <Link to={"/settings"} className="border-b-2 border-transparent hover:border-current transition duration-300 ease-in-out">
+                    SETTINGS
+                </Link>
+                <Link to={"/contact"} className="border-b-2 border-transparent hover:border-current transition duration-300 ease-in-out">
+                    CONTACT
+                </Link>
+                <Link to={"/login"} className="text-white bg-black px-8 py-3 rounded-full border-2 border-transparent hover:border-current hover:text-black hover:bg-transparent transition duration-300 ease-in-out">
+                    LOGIN
+                </Link>
             </div>
 
         </div>

4eccfcf859e1154baa4cd0be5e7ed5a4241fc769
Aja Singer <ajasinger@gmail.com>
2024-10-29 10:29:21 -0400

    import footer & styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,19 +3,21 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
-    <div>
+    <div className="h-screen">
       <Navbar />
-      <div className="p-4 h-screen flex items-center justify-center">
+      <div className="p-4 flex h-full items-center justify-center">
       <Routes>
         <Route path="/" element={ < Home /> } />
         <Route path="/signup" element={ < Signup /> } />
         <Route path="/login" element={ < Login /> } />
       </Routes>
       </div>
+      <Footer />
     </div>
   )
 }

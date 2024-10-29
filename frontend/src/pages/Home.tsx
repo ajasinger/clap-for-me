489fd3ca6b711fc8edab2f379aea5b6004f31478
Aja Singer <ajasinger@gmail.com>
2024-10-29 16:34:01 -0400

    import components

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,11 @@
+import Achievements from "../components/Achievements";
+import Calendar from "../components/Calendar";
+
 export default function Home() {
     return(
-        <div>
-            
+        <div className="w-full flex justify-between">
+            <Achievements />
+            <Calendar />
         </div>
     )
 }

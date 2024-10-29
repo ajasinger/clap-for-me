563531117a9c91f96623ef0082c4d50fe5be87e7
Aja Singer <ajasinger@gmail.com>
2024-10-29 16:33:19 -0400

    remove items-center styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <div className="p-4 flex flex-grow items-center justify-center">
+      <div className="px-36 py-12 flex flex-grow justify-center">
       <Routes>
         <Route path="/" element={ < Home /> } />
         <Route path="/signup" element={ < Signup /> } />

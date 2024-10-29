c133e90ccc3e2ad71233fdd36b2d62f1a7b4f381
Aja Singer <ajasinger@gmail.com>
2024-10-29 10:29:25 -0400

    styling

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom"
 
 export default function Navbar() {
     return(
-        <div>
-            <h1>FTW</h1>
-            <div>
-                <Link to={"/settings"}>Settings</Link>
-                <Link to={"/contact"}>Contact</Link>
-                <Link to={"/login"}>Login</Link>
+        <div className="bg-[#FFDB7D] p-6 flex items-center justify-between">
+            <h1 className="text-2xl font-black">FTW</h1>
+            <div className="flex gap-6 items-center font-semibold">
+                <Link to={"/settings"}>SETTINGS</Link>
+                <Link to={"/contact"}>CONTACT</Link>
+                <Link to={"/login"} className="text-white bg-black px-8 py-3 rounded-full">LOGIN</Link>
             </div>
 
         </div>

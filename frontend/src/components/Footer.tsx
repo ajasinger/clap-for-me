90580674698cd642ac8ed51d5a5ee934b30bfbce
Aja Singer <ajasinger@gmail.com>
2024-10-29 10:34:52 -0400

    content & styling

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom"
+
 export default function Footer() {
     return(
-        <div>
-            
+        <div className="bg-[#FFDB7D] p-12 flex justify-between">
+            <h1 className="text-2xl font-black">FTW</h1>
+            <div className="flex flex-col gap-6">
+                <Link to={"/settings"} className="hover:font-semibold">SETTINGS</Link>
+                <Link to={"/contact"} className="hover:font-semibold">CONTACT</Link>
+            </div>
+
         </div>
     )
 }

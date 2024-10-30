7fe5bed08081983cfbbfc631ea6102ce179e66ce
Aja Singer <ajasinger@gmail.com>
2024-10-30 14:01:17 -0400

    style tags

--- frontend/src/components/Achievements.tsx
+++ frontend/src/components/Achievements.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import TagDropdown from "./TagDropdown";
 
+// Data
 const tagOptionsArray = ["work", "fitness", "family"]
 
 const data = [
@@ -19,13 +20,14 @@ const data = [
                 },
                 {
                     id: 3,
-                    body: "read 1 chapter of my book.",
-                    tags: ["health"] 
+                    body: "did a presentation.",
+                    tags: ["work"] 
                 },
             ]
         }
 ]
 
+// Types
 type Achievement = {
     id: number;
     body: string;
@@ -37,6 +39,20 @@ type Achievement = {
     achievements: Achievement[];
   };
 
+// Styling
+const tagColor = (tag: string) => {
+    switch (tag) {
+        case 'work':
+          return '#BAFEC9'
+        case 'health':
+          return '#BAC5FE'
+        case 'family':
+          return '#FEBABA'
+        default:
+          return '#BAEAFE'
+      }
+}
+
 export default function Achievements() {
     const [achievements, setAchievements] = useState<Achievements[]>(data);
     const [tagOptions, setTagOptions] = useState(tagOptionsArray)
@@ -47,15 +63,21 @@ export default function Achievements() {
             <h1 className="text-xl font-bold">{achievements[0].date}</h1>
             <ol className="flex flex-col gap-6">
                 {achievements[0].achievements.map((achievement) => (
-                    <li key={achievement.id} className="flex gap-24">
+                    <li key={achievement.id} className="flex items-center gap-12">
                         {/* dropdown */}
-                        < TagDropdown tagOptions={tagOptions} setTagOptions={setTagOptions} setAchievements={setAchievements}/>
+                        < TagDropdown id={achievement.id} tagOptions={tagOptions} setTagOptions={setTagOptions} setAchievements={setAchievements}/>
                         {/* achievement */}
-                        <div className="flex gap-6">
+                        <div className="flex items-center gap-6">
                             <p>{achievement.body}</p>
                             <ul className="flex gap-2">
                                 {achievement.tags.map((tag, index) => (
-                                    <li key={index}>{tag}</li>
+                                    <li 
+                                        key={index}
+                                        style={{ backgroundColor: tagColor(tag) }}
+                                        className="px-6 py-1 rounded-full"
+                                    >
+                                        {tag}
+                                    </li>
                                 ))}
                             </ul>
                         </div>
